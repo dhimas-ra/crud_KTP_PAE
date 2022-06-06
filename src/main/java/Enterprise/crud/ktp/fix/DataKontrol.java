330d01a6827bb8d818840fdb9f43530b28273bb9
Dhimas Rizqi Akbar - 20190140112 <63139301+dhimas-ra@users.noreply.github.com>
2022-06-06 22:54:54 +0700

    'second'

--- src/main/java/Enterprise/crud/ktp/fix/DataKontrol.java
+++ src/main/java/Enterprise/crud/ktp/fix/DataKontrol.java
@@ -18,6 +18,7 @@ import java.util.Date;
 import java.util.List;
 import java.lang.String;
 import javax.servlet.http.HttpServletRequest;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
@@ -34,20 +35,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.view.RedirectView;
 
 /**
  *
  * @author Dhimas
  */
-
-
 @Controller
 public class DataKontrol {
-    
+
     DataJpaController datactrl = new DataJpaController();
     List<Data> newdata = new ArrayList<>();
-    
-    @DateTimeFormat(pattern="yyyy-MM-dd") 
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date tgllahir;
 
     @GetMapping("/data")
@@ -57,56 +57,95 @@ public class DataKontrol {
         String result = "";
         try {
             newdata = datactrl.findDataEntities().subList(0, record);
-        } catch (Exception e) {result=e.getMessage();}
+        } catch (Exception e) {
+            result = e.getMessage();
+        }
         model.addAttribute("Data", newdata);
         model.addAttribute("record", record);
-        
+
         return "database";
     }
-    
+
     @RequestMapping("/buat")
     public String createDummy(Model m) {
         Data d = new Data();
         m.addAttribute("data", d);
         return "AddData";
     }
-   
-    
-    @PostMapping(value = "/databaru", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+
+    @PostMapping(value = "/databaru", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseBody
-    public String newData(@ModelAttribute("data") Data d, @RequestParam("gambar") MultipartFile f, HttpServletRequest r)
+    public RedirectView newData(@RequestParam("foto") MultipartFile f, HttpServletRequest r)
             throws ParseException, Exception {
-        
-        
-        byte[] img = f.getBytes();
-        
-        
+        Data d = new Data();
+
+        long id = Integer.parseInt(r.getParameter("id"));
+        String noktp = r.getParameter("noktp");
+        String nama = r.getParameter("nama");
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse(r.getParameter("tgllahir"));
+        String gender = r.getParameter("gender");
+        String alamat = r.getParameter("alamat");
+        String agama = r.getParameter("agama");
+        String status = r.getParameter("status");
+        String pekerjaan = r.getParameter("pekerjaan");
+        String asal = r.getParameter("warganegara");
+        String berlaku = r.getParameter("berlakuhingga");
+        byte[] gmbr = f.getBytes();
+
+        d.setId(id);
+        d.setNoktp(noktp);
+        d.setNama(nama);
+        d.setTgllahir(date);
+        d.setGender(gender);
+        d.setAlamat(alamat);
+        d.setAgama(agama);
+        d.setStatus(status);
+        d.setPekerjaan(pekerjaan);
+        d.setWarganegara(agama);
+        d.setBerlakuhingga(berlaku);
+        d.setFoto(gmbr);
+
         datactrl.create(d);
-        return "redirect:/data";
+        return new RedirectView("/data");
     }
-    
-     @RequestMapping(value = "/gmbr", method = RequestMethod.GET, produces = {
+
+    @RequestMapping(value = "/gmbr", method = RequestMethod.GET, produces = {
         MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE
     })
     public ResponseEntity<byte[]> getImg(@RequestParam("id") long id) throws Exception {
         Data d = datactrl.findData(id);
-        byte[] img = d.getFoto();
-        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(img);
+        byte[] gmbr = d.getFoto();
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(gmbr);
     }
-    
-    @RequestMapping("/detail")
-    public String detail(@PathVariable(value = "id") long id, Model m) throws NonexistentEntityException {
-        Data d = datactrl.findData(id);
+
+    @RequestMapping("/detail/{id}")
+    public String detail(@PathVariable long id, Model m) throws NonexistentEntityException {
+        Data d = new Data();
+        
+        try{
+            d = datactrl.findData(id);
+        }catch(Exception e){
+            
+        }
+        
+        String foto = "";
+        if(d != null){
+            foto = Base64.encodeBase64String(d.getFoto());
+            m.addAttribute("foto", foto);
+        }
+        
         m.addAttribute("data", d);
+        
         return "detail";
     }
+
     @RequestMapping("/main")
     public String getMain() {
         return "menu";
     }
-    
+
     @GetMapping("/hapus/{id}")
-    
+
     public String delete(@PathVariable(value = "id") long id) throws NonexistentEntityException {
 
         datactrl.destroy(id);
@@ -119,19 +158,39 @@ public class DataKontrol {
         m.addAttribute("data", d);
         return "editktp";
     }
-    
+
     @PostMapping(value = "/perbarui", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    @ResponseBody
-    public String updateDummyData(@RequestParam("gambar") MultipartFile f, HttpServletRequest r)
+    public RedirectView updatData(@RequestParam("foto") MultipartFile f, HttpServletRequest r)
             throws ParseException, Exception {
         Data d = new Data();
-
-        int id = Integer.parseInt(r.getParameter("id"));
-        Date date = new SimpleDateFormat("yyyy-MM-dd").parse(r.getParameter("tanggal"));
-        byte[] img = f.getBytes();
         
+        long id = Integer.parseInt(r.getParameter("id"));
+        String noktp = r.getParameter("noktp");
+        String nama = r.getParameter("nama");
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse(r.getParameter("tgllahir"));
+        String gender = r.getParameter("gender");
+        String alamat = r.getParameter("alamat");
+        String agama = r.getParameter("agama");
+        String status = r.getParameter("status");
+        String pekerjaan = r.getParameter("pekerjaan");
+        String asal = r.getParameter("warganegara");
+        String berlaku = "Seumur Hidup";
+        byte[] gmbr = f.getBytes();
+
+        d.setId(id);
+        d.setNoktp(noktp);
+        d.setNama(nama);
+        d.setTgllahir(date);
+        d.setGender(gender);
+        d.setAlamat(alamat);
+        d.setAgama(agama);
+        d.setStatus(status);
+        d.setPekerjaan(pekerjaan);
+        d.setWarganegara(agama);
+        d.setBerlakuhingga(berlaku);
+        d.setFoto(gmbr);
 
         datactrl.edit(d);
-        return "updated";
+        return new RedirectView("/data");
     }
 }
